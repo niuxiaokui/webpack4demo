611dae6718df7caf61109a1256568b3c92c75cbe
niuxiaokui <1085777324@qq.com>
2019-03-30 10:50:05 +0800

    优化结构

---
+++ assets/js/styles.d99e910e1279f01f51d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{109:function(n,o,w){},110:function(n,o,w){},25:function(n,o,w){}}]);
--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (env, argv) {
-  return argv.mode === 'production' ?
-    require('./build/webpack.production') :
-    require('./build/webpack.development')
-}
